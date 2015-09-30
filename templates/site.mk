bd2fb6adcfb793af58cd5660dbf843eede252c47
Stefan Siegl <stesie@brokenpipe.de>
2015-09-30 16:23:38 +0200

    gluon release version work-around

--- templates/site.mk
+++ templates/site.mk
@@ -26,7 +26,16 @@ GLUON_SITE_PACKAGES := \
 	ffol-configurator \
 	ffol-nodewatcher
 
-DEFAULT_GLUON_RELEASE := <%= @gluon_version %>.$(shell date '+%Y%m%d')
+# The Version string should be appended seperated by a plus sign
+# so it doesn't interfere with the upstream version.  This is how
+# "opkg compare-version" works ...
+# DEFAULT_GLUON_RELEASE := <%= @gluon_version %>+$(shell date '+%Y%m%d')
+
+# ... however with Gluon 2015.1 we pushed 2015.1.20150910 et al
+# that way 2015.1.2.20150920 is considered older however :(
+# Stick to old versioning scheme til we upgread to 2015.2 soon.
+DEFAULT_GLUON_RELEASE := 2015.1.$(shell date '+%Y%m%d')
+
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
