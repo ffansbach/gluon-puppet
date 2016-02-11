c687fdea92fa1c8405635d41e663c5c59e71803b
Wolfgang Rosenauer <wr@rosenauer.org>
2016-02-11 17:21:25 +0100

    updates for gluon 2016.1

--- manifests/site_config.pp
+++ manifests/site_config.pp
@@ -1,7 +1,7 @@
 define gluon::site_config (
     $ensure                 = 'present',
     $community              = $name,
-    $gluon_version          = '2015.1.2',
+    $gluon_version          = '2016.1',
 
     $city_name              = undef,
     $site_domain            = "site.freifunk-$city_name.de",
