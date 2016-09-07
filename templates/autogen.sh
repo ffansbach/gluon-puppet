d9d701a9223a48aed8a7503de9b30b278c2db128
Wolfgang Rosenauer <wr@rosenauer.org>
2016-09-07 21:23:48 +0200

    use correct target

--- templates/autogen.sh
+++ templates/autogen.sh
@@ -20,7 +20,6 @@ make GLUON_BRANCH="$branch" GLUON_TARGET="ar71xx-generic"  $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="mpc85xx-generic" $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="x86-64" $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="x86-generic" $*
-make GLUON_BRANCH="$branch" GLUON_TARGET="x86-kvm" $*
-make GLUON_BRANCH="$branch" GLUON_TARGET="x86-kvm" $*
+make GLUON_BRANCH="$branch" GLUON_TARGET="x86-kvm_guest" $*
 
 ./propagate.sh "$branch"
