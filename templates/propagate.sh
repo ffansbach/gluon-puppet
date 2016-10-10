ad574e40b2a8304a0b48da1f9c82a7efb21bdaff
Wolfgang Rosenauer <wr@rosenauer.org>
2016-10-10 08:06:34 +0200

    add option to skip autoupdate manifest creation also provide kernel module packages

--- templates/propagate.sh
+++ templates/propagate.sh
@@ -8,12 +8,13 @@ else
 fi
 
 rm -f output/images/sysupgrade/*.manifest
-make manifest GLUON_BRANCH="$branch"
-
+if [ $NOUPDATE != 1 ]; then
+  make manifest GLUON_BRANCH="$branch"
 <% if @auto_update_seckey_file %>
-contrib/sign.sh <%= @auto_update_seckey_file %> "output/images/sysupgrade/$branch.manifest"
+  contrib/sign.sh <%= @auto_update_seckey_file %> "output/images/sysupgrade/$branch.manifest"
 <% end %>
-
+fi
 rm -rf /srv/firmware-<%= @community %>/$branch
 mkdir -p /srv/firmware-<%= @community %>/$branch/
 cp -Rapv output/images/* /srv/firmware-<%= @community %>/$branch/
+cp -Rapv output/modules/ /srv/firmware-<%= @community %>/$branch/
