d3de837057e870bd34e5643a117c1e100d84148c
Wolfgang Rosenauer <wr@rosenauer.org>
2018-02-23 21:52:58 +0100

    add new ramips-mt7621 target

--- templates/autogen.sh
+++ templates/autogen.sh
@@ -19,8 +19,9 @@ make update
 make GLUON_BRANCH="$branch" GLUON_TARGET="ar71xx-tiny"  $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="ar71xx-generic"  $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="mpc85xx-generic" $*
+make GLUON_BRANCH="$branch" GLUON_TARGET="ramips-mt7621" $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="x86-64" $*
-make GLUON_BRANCH="$branch" GLUON_TARGET="x86-generic" $*
-make GLUON_BRANCH="$branch" GLUON_TARGET="x86-geode" $*
+#make GLUON_BRANCH="$branch" GLUON_TARGET="x86-generic" $*
+#make GLUON_BRANCH="$branch" GLUON_TARGET="x86-geode" $*
 
 ./propagate.sh "$branch"
